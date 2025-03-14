13fd4155883f3b3747df434451fd8912203b442b
tufailfahad01 <tufailfahad01@gmail.com>
2025-03-14 17:24:55 +0500

    LayOut

--- src/components/core/Header/Layout/Layout.tsx
+++ src/components/core/Header/Layout/Layout.tsx
@@ -1,16 +1,25 @@
 import { ReactNode } from "react";
+import { useState } from "react";
 import { Box } from "@mui/material";
 import Header from "../Header";
+import Sidebar from "@/pages/components/Sidebar";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const CustomLayout = ({ children }: LayoutProps) => {
+
+const [sidebarOpen, setSidebarOpen] = useState(true);
+
   return (
     <Box sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
       <Header />
-      <Box sx={{ flex: 1 }}>{children}</Box>
+
+      <Box  sx={{ display: 'flex', height: "100vh" }}>
+        <Sidebar />
+        <Box sx={{ flex: 1 }}>{children}</Box>
+      </Box>
     </Box>
   );
 };
