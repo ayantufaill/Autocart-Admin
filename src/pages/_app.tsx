a35151b66681e6f07aea9a526a35f167820ebec9
ayantufail <ayantufail.tdc@gmail.com>
2025-03-14 12:13:01 +0500

    header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+
+import store from "@/redux/store";
+import Layout from "@/components/core/Header/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </div>
+  );
 }
---
+++ src/pages/admin/ads/index.tsx
@@ -0,0 +1,10 @@
+import ColorTabs from "@/components/common/ColorTabs/ColorTabs";
+import React from "react";
+
+export default function index() {
+  return (
+    <div>
+      <ColorTabs />
+    </div>
+  );
+}
