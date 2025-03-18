311fcf8e7e5dace10f5e79a4931be44582c1f39b
tufailfahad01 <tufailfahad01@gmail.com>
2025-03-18 15:18:16 +0500

    MessageManagement

---
+++ src/components/common/Messagemanagement/Flaggedmessage.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+
+interface FlaggedMessageProps {
+    title: string;
+    value: number;
+    percentage: string;
+    description: string;
+}
+
+const FlaggedMessage: React.FC<FlaggedMessageProps> = ({ title, value, percentage, description }) => {
+    return (
+        <Box
+            sx={{
+                // width: '242px',
+                height: '124px',
+                bgcolor: '#FFFFFF',
+                borderRadius: '6px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-evenly',
+                alignItems: 'center'
+            }}
+        >
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '206px' }}>
+                <Typography sx={{ color: '#854D0E', fontSize: '15px' }}>{title}</Typography>
+                <Box
+                    sx={{
+                        p: '1px',
+                        height: '23px',
+                        width: '23px',
+                        border: '0.5px solid #CACACA',
+                        bgcolor: 'white',
+                        borderRadius: '6px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
+                >
+                    <CalendarMonthOutlinedIcon sx={{ color: '#9CA3AF', width: '15px', height: '15px' }} />
+                </Box>
+            </Box>
+
+            <Box sx={{ display: 'flex', flexDirection: 'column', width: '206px', gap: '6px', justifyContent: 'center', height: '51px' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Typography sx={{ color: '#1F2937', fontSize: '24px', fontWeight: 600 }}>{value}</Typography>
+                    <Typography sx={{ color: percentage.includes("+") ? "#22C55E" : "#EF4444", fontSize: '11px' }}>{percentage}</Typography>
+                </Box>
+                <Typography sx={{ fontSize: '12px', color: '#9CA3AF' }}>{description}</Typography>
+            </Box>
+        </Box>
+    );
+};
+
+export default FlaggedMessage;
--- src/pages/admin/messagemanagement/index.tsx
+++ src/pages/admin/messagemanagement/index.tsx
@@ -1,68 +1,98 @@
-import React from 'react'
-import { Box, Typography, Grid } from '@mui/material'
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import React from 'react';
+import { Box, Typography, Grid,InputAdornment, } from '@mui/material';
+import FlaggedMessage from '@/components/common/Messagemanagement/Flaggedmessage';
+import FinanceOverviewTabs from "@/components/common/FinanceTabs/FinanceOverviewTabs";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import TextField from '@mui/material/TextField';
 
-const index = () => {
+const data = [
+    { id: 1, title: "Today", value: 54, percentage: "+9.2%", description: "Compared to yesterday" },
+    { id: 2, title: "This Week", value: 25455, percentage: "-9.2%", description: "Compared to Last Week" },
+    { id: 3, title: "This Month", value: 347588, percentage: "+9.2%", description: "Compared to Last Month" },
+    { id: 4, title: "This Year", value: 875223, percentage: "+9.2%", description: "Compared to Last Year" },
+];
 
-    const data = [
-        { id: 1, title: "Today", value: 54, percentage: "+9.2%" , description: "Compared to yesterday"},
-        { id: 2, title: "This Weak", value: 25455, percentage: "-9.2%" , description: "Compared to Last Weak"},
-        { id: 3, title: "This Month", value: 347588, percentage: "+9.2%" , description: "Compared to Last Month"},
-        { id: 4, title: "This Year", value: 875223, percentage: "+9.2%" , description: "Compared to Last Year"},
+const reported = [
+    { id: 1, title: "Today", value: 54, percentage: "+9.2%", description: "Compared to yesterday" },
+    { id: 2, title: "This Week", value: 25455, percentage: "-9.2%", description: "Compared to Last Week" },
+    { id: 3, title: "This Month", value: 347588, percentage: "+9.2%", description: "Compared to Last Month" },
+    { id: 4, title: "This Year", value: 875223, percentage: "+9.2%", description: "Compared to Last Year" },
+];
+
+const FlaggedMessagesGrid = () => {
+
+    const [activeTab, setActiveTab] = React.useState<string>("Message Overview");
+
+    const tabs = [
+        { label: "Message Overview" },
+        { label: "Flagged Messages" },
+        { label: "Reported Messages" },
     ];
 
     return (
-        <Box
-            sx={{
-                bgcolor: '#f9f9f9',
-                px: '45px',
-                py: "18px"
-            }}
-        >
-            <Box
-                sx={{ display: 'flex', flexDirection: 'column', gap: '28px' }}
-            >
-                <Box>
-                    <Typography sx={{ color: "#1F2937", fontSize: '24px', fontWeight: 600 }}>Flagged Messages</Typography>
-                </Box>
-                <Box sx={{ px: '6px' , gap:'28px'}}>
-                    <Grid container spacing={2}>
-                        {data.map((item) => (
-                            <Grid item xs={12} sm={6} md={6} lg={3} key={item.id}>
-                                <Box sx={{
-                                    width: '242px',
-                                    height: '124px',
-                                    bgcolor: '#FFFFFF',
-                                    borderRadius: '6px',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'space-evenly',
-                                    alignItems: 'center'
-                                }}>
-                                    <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '206px' }}>
-                                        <Typography sx={{ color: '#854D0E', fontSize: '15px' }}>{item.title}</Typography>
-                                        <Box sx={{ p: '1px', height: '23px', width: "23px", border:'0.5px solid #CACACA', bgcolor:'white', borderRadius:'6px' , display:'flex', justifyContent:'center', alignItems:'center'}}>
-                                            <CalendarMonthOutlinedIcon sx={{color:'#9CA3AF', bgcolor:"white", width:'15px', height:'15px'}}/>
-                                        </Box>
-                                    </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '6px', bgcolor: '#f9f9f9', px: '60px', }}>
 
-                                    <Box sx={{ display: 'flex', flexDirection: 'column', width: '206px', gap: '6px', justifyContent: 'center', height: '51px' }}>
-                                        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                            <Typography sx={{ color: '#1F2937', fontSize: '24px', fontWeight: 600 }}>{item.value}</Typography>
-                                            <Typography sx={{ color: item.percentage.includes("+") ? "#22C55E" : "#EF4444", fontSize: '11px' }}>{item.percentage}</Typography>
-                                        </Box>
-                                        <Box>
-                                            <Typography sx={{ fontSize: '12px', color: '#9CA3AF' }}>{item.description}</Typography>
-                                        </Box>
-                                    </Box>
-                                </Box>
-                            </Grid>
-                        ))}
-                    </Grid>
+            <Box sx={{ py: '24px', display: 'flex', flexDirection: 'column', gap: '36px', }}>
+
+                <Box sx={{ height: { xs: '150px', md: '80px', lg: '46px' } }}>
+                    <FinanceOverviewTabs tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} />
+                </Box>
+
+                <TextField
+                    placeholder="Search Ads"
+                    variant="outlined"
+                    sx={{
+                        marginBottom: 2,
+                        backgroundColor: "#F9F9F9",
+                        width: {xs:'100%',lg:"600px"},
+                        borderRadius: "10px",
+                        "& .MuiOutlinedInput-root": {
+                            borderRadius: "10px", 
+                        }
+                    }}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchOutlinedIcon sx={{ color: "#9CA3AF" }} />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+            </Box>
+
+
+            <Box
+                sx={{ display: 'flex', flexDirection: 'column', gap: '25px', bgcolor: '#f9f9f9' }}
+            >
+                <Box sx={{ bgcolor: '#f9f9f9', py: '18px' }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '28px' }}>
+                        <Typography sx={{ color: '#1F2937', fontSize: '24px', fontWeight: 600 }}>Flagged Messages</Typography>
+                        <Grid container spacing={2}>
+                            {data.map((item) => (
+                                <Grid item xs={12} sm={6} md={6} lg={3} key={item.id}>
+                                    <FlaggedMessage {...item} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Box>
+                </Box>
+
+                <Box sx={{ bgcolor: '#f9f9f9', py: '18px' }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '28px' }}>
+                        <Typography sx={{ color: '#1F2937', fontSize: '24px', fontWeight: 600 }}>Reported Messages</Typography>
+                        <Grid container spacing={2}>
+                            {reported.map((item) => (
+                                <Grid item xs={12} sm={6} md={6} lg={3} key={item.id}>
+                                    <FlaggedMessage {...item} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Box>
                 </Box>
             </Box>
         </Box>
-    )
-}
 
-export default index
+    );
+};
+
+export default FlaggedMessagesGrid;
